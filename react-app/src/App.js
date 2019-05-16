9c9ac54c41688cf5848dfabfa550b0956cf49250
ognemrak <oleg.sivalnyov@gmail.com>
2019-05-16 22:37:53 +0400

    header changes

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,26 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<Header />
+		</div>
+	);
+}
+
+function Header() {
+	return (
+		<div className="Header">
+			<div className="container">
+				<img src="https://dummyimage.com/180x60/000/fff" alt="logo" />
+				<ul className="main-menu">
+					<li><a href="http://" target="_blank" rel="noopener noreferrer">Text</a></li>
+					<li><a href="http://" target="_blank" rel="noopener noreferrer">Test</a></li>
+					<li><a href="http://" target="_blank" rel="noopener noreferrer">Text2</a></li>
+					<li><a href="http://" target="_blank" rel="noopener noreferrer">Texst2</a></li>
+				</ul>
+			</div>
+		</div>
+	)
 }
 
 export default App;
